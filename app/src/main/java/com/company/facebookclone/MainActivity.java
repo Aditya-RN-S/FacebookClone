07d5ef8bf106fc8bc3c3ade346108701b0c1c487
Aditya-RN-S <adityaprajapati8943@gmail.com>
2026-06-16 20:05:47 +0530

    toolbar Added

--- app/src/main/java/com/company/facebookclone/MainActivity.java
+++ app/src/main/java/com/company/facebookclone/MainActivity.java
@@ -2,9 +2,12 @@ package com.company.facebookclone;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -30,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toolbar toolbar = setToolbar();
+
+
+
+
 
 
         feedRecyclerView = findViewById(R.id.feedRecyclerView);
@@ -116,5 +125,62 @@ public class MainActivity extends AppCompatActivity {
          post3.setTime("Yesterday");
          post3.setPostText("Just finished implementing multiple view types in an Android RecyclerView. Feels amazing!");
          samplePosts.add(post3);
+
+     }
+
+     private Toolbar setToolbar(){
+         Toolbar toolbar = findViewById(R.id.toolbar);
+         setSupportActionBar(toolbar);
+
+         if (getSupportActionBar()!=null){
+             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+             getSupportActionBar().setTitle(R.string.facebook_toolbar_title);
+         }
+
+         toolbar.setLogo(R.drawable.ic_facebook_logo);
+         toolbar.setTitle(R.string.facebook_toolbar_title);
+         toolbar.setTitleTextColor(getResources().getColor(R.color.fb_blue,getTheme()));
+
+
+
+         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+             @Override
+             public boolean onMenuItemClick(MenuItem item) {
+                 if (item.getItemId()==R.id.messenger_menu){
+                     return true;
+                 } else if (item.getItemId()==R.id.seach_menu) {
+                     return true;
+                 } else if (item.getItemId()==android.R.id.home) {
+                     getOnBackPressedDispatcher().onBackPressed();
+                     return true;
+                 }
+
+                 return false;
+             }
+
+         });
+
+         return toolbar;
+     }
+
+
+     @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+     }
+
+     @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+         if (item.getItemId()==R.id.messenger_menu){
+             return true;
+         } else if (item.getItemId()==R.id.seach_menu) {
+             return true;
+         } else if (item.getItemId()==android.R.id.home) {
+             getOnBackPressedDispatcher().onBackPressed();
+             return true;
+         }
+
+         return false;
      }
 }
